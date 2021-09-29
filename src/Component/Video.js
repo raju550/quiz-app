23c881e6a10199c0189c87caeb12da8b7e774f6b
md ismail hossain <ismailtex043@gmail.com>
2021-09-29 23:05:34 +0600

    firebase database

--- src/Component/Video.js
+++ src/Component/Video.js
@@ -1,15 +1,18 @@
 import React from "react";
 import image from "../assets/images/3.jpg";
 import classes from "../styles/Video.module.css";
-export default function Video() {
+export default function Video({ title, id, noq }) {
   return (
     <div>
       <div className={classes.video}>
-        <img src={image} alt="" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+        <img
+          src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`}
+          alt={title}
+        />
+        <p>{title}</p>
         <div className={classes.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
+          <p>{noq} Questions</p>
+          <p>Total: {noq * 5}</p>
         </div>
       </div>
     </div>
--- src/Component/Videos.js
+++ src/Component/Videos.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import classes from "../styles/Videos.module.css";
+import useVideoList from "../Hooks/useVideoList";
 import Video from "./Video";
+import InfiniteScroll from "react-infinite-scroll-component";
 export default function Videos() {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideoList(page);
+
   return (
-    <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video></Video>
-      </Link>
-      <Link to="/quiz">
-        <Video></Video>
-      </Link>
-      <Link to="/quiz">
-        <Video></Video>
-      </Link>
-      <Link to="/quiz">
-        <Video></Video>
-      </Link>
-      <Link to="/quiz">
-        <Video></Video>
-      </Link>
-      <Link to="/quiz">
-        <Video></Video>
-      </Link>
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          loader="loading..."
+          next={() => setPage(page + 8)}
+        >
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                ></Video>
+              </Link>
+            ) : (
+              <Video
+                title={video.title}
+                id={video.youtubeID}
+                noq={video.noq}
+              ></Video>
+            )
+          )}
+        </InfiniteScroll>
+      )}
+      {!loading && videos.length === 0 && <div>No data avilable</div>}
+      {error && <div>There was a error</div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
